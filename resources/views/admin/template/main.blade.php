e3b5e55ff9636ac9e2d66f4c2a9185586779bf9a
ImbarInformatika <imbarps16@gmail.com>
2024-06-16 04:51:00 +0800

    pinjam buku

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -21,11 +21,6 @@
     <!-- Google Font -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" --}}
-    {{-- integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> --}}
-
-
     @stack('style')
 </head>
 
--- resources/views/admin/template/sidebar.blade.php
+++ resources/views/admin/template/sidebar.blade.php
@@ -3,14 +3,6 @@
     <section class="sidebar">
         <!-- Sidebar user panel -->
         <div class="user-panel">
-            {{-- <div class="pull-left image">
-                <img src="{{ asset('template') }}/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-            </div>
-            
-            <div class="pull-left info">
-                <p>Admin</p>
-                <p>Imbar</p>
-            </div> --}}
         </div>
 
         <ul class="sidebar-menu" data-widget="tree">
@@ -19,21 +11,25 @@
                     <i class="fa fa-th"></i> <span>Dashboard</span>
                 </a>
             </li>
-            <li>
-                <a href="{{ route('daftar-buku') }}">
-                    <i class="fa fa-book"></i> <span>Daftar Buku</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('daftar-penerbit') }}">
-                    <i class="fa fa-pencil"></i> <span>Daftar Penerbit</span>
-                </a>
-            </li>
+
             <li>
                 <a href="{{ route('daftar-mahasiswa') }}">
                     <i class="fa fa-users"></i> <span>Daftar Mahasiswa</span>
                 </a>
             </li>
+
+            <li>
+                <a href="{{ route('daftar-penerbit') }}">
+                    <i class="fa fa-pencil"></i> <span>Daftar Penerbit</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="{{ route('daftar-buku') }}">
+                    <i class="fa fa-book"></i> <span>Daftar Buku</span>
+                </a>
+            </li>
+
             <li>
                 <a href="{{ route('daftar-peminjaman') }}">
                     <i class="fa fa-list-alt"></i> <span>Daftar Peminjaman</span>
